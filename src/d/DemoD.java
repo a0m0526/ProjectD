8ca101e278810abb7ddea37304e8d070cdc83e75
Ross <Ross@192.168.1.237>
2024-01-03 19:59:16 -0600

    Initial Commit

---
+++ src/d/DemoD.java
@@ -0,0 +1,13 @@
+package d;
+
+public class DemoD {
+	
+	public static void main(String[] args) {
+		
+		System.out.println("Demo D");
+		
+		System.out.println("whatever1");
+		System.out.println("Whatever2");
+	}
+
+}
